be78d3d13f6640281394bae20a916e0c2e06d19f
BelodonaQ <96733956+BelodonaQ@users.noreply.github.com>
2026-06-16 16:08:41 +0300

    day 2 mandatory challenge completed

---
+++ Week1/day2/ExerciseXP/xp.py
@@ -0,0 +1,67 @@
+print(f"Hello World" * 4)
+
+# 2
+
+x = 99**3*8
+print(x) #7762392
+
+#3 (Not sure what the question asks of me so i'll skip)
+
+#4
+
+computer_brand = "Apple"
+
+print(f"i have an {computer_brand} computer") # i have an apple computer
+
+#5
+name = "Seem"
+age = 28
+shoe_size = 45
+info = f"Hi my name is {name} and i'm {age}. Btw, i wear a size {shoe_size}"
+print(info) 
+
+# prints expected result
+
+#6
+
+a = 6
+b = 7
+
+if b <= a:
+  print('Hi World')
+else:
+  print("Hello World")
+
+#7
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#8
+
+name = input(str(" Enter your name "))
+my_name = "Seem"
+
+if name == my_name:
+   print("You're an imposter!")
+else:
+   print("You're good fam")
+  
+
+#9 
+
+height = float(input(" Your height in cm "))
+# cm_max_height = 145
+
+
+if height == 145:
+   print("You almost lost boss, but enjoy!")
+elif height >145:
+   print("Enjoy the ride!")
+else:
+   print("Get height increasing surgery friend")
+
