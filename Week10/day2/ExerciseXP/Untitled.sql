c94b5959c1501de216b38ccef8d226e9aa69574a
Seemkiyee Radcliffe <seemkiyeeradcliffe@Seemkiyees-MacBook-Pro.local>
2026-08-23 13:53:47 +0300

    the sql exercise

---
+++ Week10/day2/ExerciseXP/Untitled.sql
@@ -0,0 +1,49 @@
+CREATE TABLE items (
+	item_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	item_name VARCHAR(100) NOT NULL,
+	price INTEGER NOT NULL CHECK (price >= 0)
+)
+
+CREATE TABLE customers (
+	customer_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL
+)
+
+INSERT INTO items (item_name, price)
+VALUES
+	('Small Desk', 100),
+	('Large Desk', 300),
+	('Fan', 80);
+
+INSERT INTO customers (first_name, last_name)
+VALUES
+	('Greg', 'Jones'),
+	('Sandra', 'Jones'),
+	('Scott', 'Scott'),
+	('Trevor', 'Green'),
+	('Melanie', 'Johnston')
+
+SELECT * 
+FROM items
+
+SELECT * 
+FROM items
+WHERE price > 80;
+
+SELECT *
+FROM items
+WHERE price <= 300;
+
+SELECT *
+FROM customers
+WHERE last_name = 'Smith';
+
+SELECT *
+FROM customers
+WHERE last_name = 'Jones';
+
+SELECT *
+FROM customers
+WHERE  first_name <> 'Scott';
+
